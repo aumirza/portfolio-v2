d5a7d9274ccdbad5ca120f753c7415910b5ce284
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2024-06-10 12:57:24 +0530

    Fix: Card design

--- src/components/ProjectGridCard.jsx
+++ src/components/ProjectGridCard.jsx
@@ -5,9 +5,9 @@ export const ProjectGridCard = ({ project }) => {
   const { title, description, cover } = project
 
   return (
-    <div className="max-w-[20rem] mx-auto p-2 bg-primary text-secondary rounded-lg shadow-md hover:-translate-y-3 hover:shadow-xl flex flex-col items-center transition-all ease-in-out duration-500">
+    <div className="w-[22rem] h-[28rem] mx-auto p-5 bg-primary text-secondary rounded-3xl shadow-md hover:-translate-y-3 hover:shadow-xl flex flex-col items-center transition-all ease-in-out duration-500">
       {/* <div className="z-20 group h-52 w-[98%] flex justify-center items-center hover:bg-transparent bg-secondary dark:bg-primary  overflow-hidden"> */}
-      <div className="z-20 group h-52 w-[98%] flex justify-center items-center hover:bg-transparent   overflow-hidden">
+      <div className="z-20 group h-60 flex justify-center items-center hover:bg-transparent   overflow-hidden">
         <img
           className="h-full object-cover rounded-md mix-blend-multiply"
           // className="h-full object-cover rounded-md mix-blend-multiply group-hover:grayscale-0 group-hover:brightness-100 grayscale brightness-90"
@@ -20,10 +20,10 @@ export const ProjectGridCard = ({ project }) => {
         <h1 className="text-2xl font-semibold">{title}</h1>
       </div>
       <div className="px-3 mt-2 mb-5">
-        <p className="text-sm leading-3 font-mono">
-          {description.substring(0, 150) +
-            (description.length > 150 ? "  ...  " : "") +
-            (description.length > 150
+        <p className="text-lg leading-5 text-center">
+          {description.substring(0, 80) +
+            (description.length > 80 ? "  ...  " : "") +
+            (description.length > 80
               ? description.substring(
                   description.length - 15,
                   description.length
@@ -31,18 +31,23 @@ export const ProjectGridCard = ({ project }) => {
               : "")}
         </p>
       </div>
-      <div className="mb-1 px-2 flex items-center justify-between w-full">
+      <div className="flex items-center gap-10">
         {project.source && (
-          <a href={project.source} target="_blank" rel="noreferrer">
-            <FaGithub className="text-xl" />
+          <a
+            href={project.source}
+            className="hover:text-white"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub className="text-3xl" />
           </a>
         )}
         {project.url && (
           <a
             href={project.url}
-            className="border text-secondary border-secondary hover:bg-secondary hover:text-primary px-2 rounded-md"
+            className="border-2 text-secondary border-secondary hover:bg-secondary hover:text-primary px-5 py-2  rounded-full"
           >
-            <span className="text-sm">View</span>
+            <span className="text-lg">View</span>
           </a>
         )}
       </div>
