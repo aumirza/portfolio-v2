e96c81f77c655f04d0adcbf78b4c6094dfb65b09
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-02 21:34:22 +0530

    feat: conditionally render snowfall effect based on season

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,17 +6,21 @@ import snowflake from "../images/white-snowflake.png"
 import { Footer } from "./layouts/Footer"
 import { Header } from "./layouts/Header"
 
+const isWinter = [11, 12, 0, 1].includes(new Date().getMonth())
+
 export const Layout = ({ children, className }) => {
   const snowflakeImage = new Image()
   snowflakeImage.src = snowflake
   return (
     <div className="dark:bg-secondary dark:text-white ">
-      <Snowfall
-        images={[snowflakeImage]}
-        snowflakeCount={100}
-        radius={[2, 10]}
-        style={{ position: "fixed", top: 0, left: 0, right: 0, bottom: 0 }}
-      />
+      {isWinter && (
+        <Snowfall
+          images={[snowflakeImage]}
+          snowflakeCount={100}
+          radius={[2, 10]}
+          style={{ position: "fixed", top: 0, left: 0, right: 0, bottom: 0 }}
+        />
+      )}
       <Header />
       <main className={className}>{children}</main>
       <Footer />
