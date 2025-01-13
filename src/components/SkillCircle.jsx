e5edff979e9a0a70c8c08cc831245db958e8e23c
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-13 20:05:03 +0530

    fix: update className concatenation in SkillCircle for improved readability

--- src/components/SkillCircle.jsx
+++ src/components/SkillCircle.jsx
@@ -5,7 +5,7 @@ export const SkillCircle = ({ name, icon, level, className }) => {
     <div className="flex flex-col items-center hover:cursor-pointer hover:scale-105">
       <div className="flex items-center justify-center mb-1 rounded bg-card w-14 h-14 md:w-16 md:h-16">
         <img
-          className={"w-10 h-10 md:h-12 md:w-12" + " " + className}
+          className={`w-10 h-10 md:h-12 md:w-12 ${className}`}
           src={icon}
           alt={name}
         />
