312847749c511797eeaf552e9caa437551de33f5
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-24 23:09:26 +0530

    feat: sort experiences by start date in useExperienceQuery hook

--- src/hooks/useExperienceQuery.js
+++ src/hooks/useExperienceQuery.js
@@ -22,12 +22,18 @@ export const useExperienceQuery = () => {
     }
   `)
 
-  const experiences = data.allMarkdownRemark.edges.map(
+  let experiences = data.allMarkdownRemark.edges.map(
     ({ node: { id, frontmatter } }) => ({
       id,
       ...frontmatter,
     })
   )
+  
+  experiences = experiences.sort((a, b) => {
+    const dateA = new Date(a.started)
+    const dateB = new Date(b.started)
+    return dateB - dateA
+  })
 
   return { experiences }
 }
