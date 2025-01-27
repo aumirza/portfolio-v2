73207b9fde78e5d9221c705bb278a442976c725b
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-27 23:18:16 +0530

    feat: implement project filtering with clear option in ProjectsFilter component

--- src/components/ProjectsFilter.jsx
+++ src/components/ProjectsFilter.jsx
@@ -1,7 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 
 export const ProjectsFilter = ({ projectsData, setProjects }) => {
+  // const [selectedFilters, setSelectedFilters] = React.useState([])
+
+  const [activeFilter, setActiveFilter] = useState("")
+
+  const clearFilter = () => {
+    setActiveFilter("")
+    setProjects(projectsData)
+  }
+
   const filterProjects = stack => {
+    // if (selectedFilters.includes(stack)) {
+    //   setSelectedFilters(selectedFilters.filter(filter => filter !== stack))
+    // } else {
+    //   setSelectedFilters([...selectedFilters, stack])
+    // }
+
+    setActiveFilter(stack.name)
     const filteredProjects = projectsData.filter(project => {
       const projectStack = project.project.fields.techStack
       return projectStack && projectStack.some(item => item.name === stack.name)
@@ -23,17 +39,42 @@ export const ProjectsFilter = ({ projectsData, setProjects }) => {
       {uniqueStack.map(stack => (
         <button
           key={stack.name}
-          className="flex gap-0.5 bg-card rounded-full p-2 py-1 cursor-pointer"
+          className={
+            "flex justify-center items-center gap-0.5 rounded-full px-3 py-2 cursor-pointer transition-all duration-200 ease-in-out hover:scale-105 " +
+            (activeFilter === stack.name ? "bg-primary" : "bg-card ")
+          }
           onClick={e => filterProjects(stack)}
         >
           <img
-            className="size-6 filter brightness-0"
+            className="rounded size-5 filter brightness-0"
             src={stack.icon}
             alt={stack.name}
           />
           <p className="text-sm text-gray-900">{stack.name}</p>
         </button>
       ))}
+      {activeFilter && (
+        <button
+          className="flex gap-0.5 rounded-full p-2 py-1 cursor-pointer bg-primary text-gray-900"
+          onClick={e => clearFilter()}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="12" cy="12" r="10" />
+            <path d="m15 9-6 6" />
+            <path d="m9 9 6 6" />
+          </svg>
+        </button>
+      )}
     </div>
   )
 }
